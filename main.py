2f9f7368dc545f873c96e2554c7c49b425bf2a9e
Anastasia <grey-mice@mail.ru>
2023-12-23 13:21:12 +0300

    Поменяла порядок вывода транзакции. Стал порядок по убыванию даты.

--- main.py
+++ main.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
         executed_date = get_only_executed_state(operations_date)
         sorted_by_date = get_sort_by_date(executed_date)
         sorted_by_date_last_5 = sorted_by_date[-5:]
+        sorted_by_date_last_5 = list(reversed(sorted_by_date_last_5))
         for trans in sorted_by_date_last_5:
             print_trans(trans)
