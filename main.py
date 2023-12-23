f5e5d86ea3f37d0e8b9dbe19454dccf25589a339
Anastasia <grey-mice@mail.ru>
2023-12-23 12:59:35 +0300

    Основная программа, которая выводит пять 5 последних транзакции из файла operations.json с помощью функций из файла functions.py.

---
+++ main.py
@@ -0,0 +1,11 @@
+from functions import get_only_executed_state, get_sort_by_date, print_trans
+import json
+
+if __name__ == "__main__":
+    with open("operations.json") as file:
+        operations_date = json.load(file)
+        executed_date = get_only_executed_state(operations_date)
+        sorted_by_date = get_sort_by_date(executed_date)
+        sorted_by_date_last_5 = sorted_by_date[-5:]
+        for trans in sorted_by_date_last_5:
+            print_trans(trans)
